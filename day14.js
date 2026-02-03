21c8e26e17fa4db4ebe39f408ab22a958cef6ce0
chinmayd1 <chinmay.minskole@gmail.com>
2026-02-03 08:07:03 +0530

    added

---
+++ day14.js
@@ -0,0 +1,23 @@
+
+let a = 10
+console.log(a)
+console.log(typeof a)
+
+let b = true
+console.log(b)
+console.log(typeof b)
+
+let c = "chinmay"
+console.log(c)
+console.log(typeof c)
+
+let names = ["samay","raj","sarthak","sarika"]
+console.log(names)
+console.log(typeof names)
+
+let info = {
+    firstName:"chinmay",
+    lastName:"deshpande",
+    age:23
+
+}
